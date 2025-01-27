065e09398be48203c243907299802d1c9adc5c19
taniguhey <whole.creation.mail@gmail.com>
2025-01-27 16:38:26 +0900

    戻り値を削除
    ハンズオンの途中で変更するため初心者でも変更できるように

--- src/app/Aggregates/Account.php
+++ src/app/Aggregates/Account.php
@@ -29,9 +29,8 @@ class Account extends Projection
      *
      * @param string $uuid UUID
      * @param string $userName ユーザー名
-     * @return Account
      */
-    public static function create(string $uuid, string $userName): Account {
+    public static function create(string $uuid, string $userName) {
         // 口座作成イベントを発行
         event(new AccountCreated($uuid, $userName));
 
@@ -49,9 +48,8 @@ class Account extends Projection
      * 入金を行う
      *
      * @param int $amount 入金額
-     * @return void
      */
-    public function addMoney(int $amount): void {
+    public function addMoney(int $amount) {
         // 残高に入金
         $this->balance += $amount;
 
@@ -62,9 +60,8 @@ class Account extends Projection
      * 出金を行う
      *
      * @param int $amount 出金額
-     * @return void
      */
-    public function subtractMoney(int $amount): void {
+    public function subtractMoney(int $amount) {
         // TODO: 残高から出金
 
         // TODO: 出金イベントを発行
